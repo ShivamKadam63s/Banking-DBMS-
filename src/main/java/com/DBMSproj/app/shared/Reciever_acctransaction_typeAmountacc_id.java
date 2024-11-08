2304b6717eafac3b012ec6fb13580286a57676ac
RuchirKadam1729 <ruchirkadam1729@gmail.com>
2024-11-08 14:38:16 +0530

    Delete src directory

--- src/main/java/com/DBMSproj/app/shared/Reciever_acctransaction_typeAmountacc_id.java
+++
@@ -1,8 +0,0 @@
-package com.DBMSproj.app.shared;
-import java.math.BigDecimal;
-public record Reciever_acctransaction_typeAmountacc_id(
-    Long Reciever_acc,
-    String transaction_type,
-    BigDecimal Amount,
-    Long acc_id
-) {}
